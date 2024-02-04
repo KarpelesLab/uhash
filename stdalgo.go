761f79c4353e12f1951d7a701292df43e2b5e00d
Mark Karpeles <magicaltux@gmail.com>
2024-02-04 12:34:37 +0900

    add shake128/shake256

--- stdalgo.go
+++ stdalgo.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
+	"hash"
 
 	"golang.org/x/crypto/md4"
 	"golang.org/x/crypto/ripemd160"
@@ -22,11 +23,13 @@ func init() {
 	reg(&algo{name: "sha512", desc: "SHA512", factory: sha512.New})
 	reg(&algo{name: "sha512-224", desc: "SHA512-224", factory: sha512.New512_224})
 	reg(&algo{name: "sha512-256", desc: "SHA512-256", factory: sha512.New512_256})
-	reg(&algo{name: "ripemd-160", alias: []string{"ripemd", "ripemd160"}, desc: "RIPE Message Digest", factory: ripemd160.New})
+	reg(&algo{name: "ripemd-160", alias: []string{"ripemd", "ripemd160", "rmd160"}, desc: "RIPE Message Digest", factory: ripemd160.New})
 	reg(&algo{name: "sha3-224", desc: "SHA3-224", factory: sha3.New224})
 	reg(&algo{name: "sha3-256", desc: "SHA3-256", factory: sha3.New256})
 	reg(&algo{name: "sha3-384", desc: "SHA3-384", factory: sha3.New384})
 	reg(&algo{name: "sha3-512", desc: "SHA3-512", factory: sha3.New512})
+	reg(&algo{name: "shake128", desc: "SHA3 SHAKE128", factory: func() hash.Hash { return sha3.NewShake128() }})
+	reg(&algo{name: "shake256", desc: "SHA3 SHAKE256", factory: func() hash.Hash { return sha3.NewShake256() }})
 	reg(&algo{name: "keccak256", desc: "Keccak-256 (legacy sha3)", factory: sha3.NewLegacyKeccak256})
 	reg(&algo{name: "keccak512", desc: "Keccak-512 (legacy sha3)", factory: sha3.NewLegacyKeccak512})
 }
